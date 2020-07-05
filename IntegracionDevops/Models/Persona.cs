404e634930b2cb00b0d8bf0c52971e365bd74217
pjrace3345 <pjrace33@gmail.com>
2020-07-05 15:50:13 -0500

    segundo commit

---
+++ IntegracionDevops/Models/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegracionDevops.Models
+{
+    public class Persona
+    { 
+
+        public int id { get; set; }
+        public string nombre { get; set; }
+
+    }
+}
